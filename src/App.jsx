9059337b3fe43588a2d0bd707dde284b7521e076
ahmedaldailami <ahmedadnan2024@gmail.com>
2026-03-02 03:45:52 +0300

    init the project

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+
+function App() {
+
+
+  return (
+    <div className="">
+
+      {/* <!-- Animated Background --> */}
+      <div className="background"></div>
+      <div className="orb orb-1"></div>
+      <div className="orb orb-2"></div>
+      <div className="orb orb-3"></div>
+
+
+    </div>
+  )
+}
+
+export default App
