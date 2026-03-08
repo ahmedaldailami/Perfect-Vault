757134391dedd7d207642bae0369b837e9f738fe
ahmedaldailami <ahmedadnan2024@gmail.com>
2026-03-08 03:59:08 +0300

    Stats Cards

---
+++ src/features/markets/components/utils/Stats/Stats.config.js
@@ -0,0 +1,16 @@
+export const StatsData = [
+  {
+    label: "Total Market Cap",
+    value: "$2.48T",
+    change: "+2.4%",
+  },
+
+  {
+    label: "24h Volume",
+    value: "$84.2B",
+    change: "+5.8 %",
+  },
+  { label: "BTC Dominance", value: "52.4%", change: "-0.3 % " },
+
+  { label: "Active Cryptos", value: "12,847", change: "+24" },
+];
--- src/features/markets/pages/Markets.jsx
+++ src/features/markets/pages/Markets.jsx
@@ -1,28 +1,15 @@
-import StatCard from "../components/StatCard";
+import StatCard from "../components/utils/Stats/StatCard";
+import { StatsData } from "../components/utils/Stats/Stats.config";
 
 const Markets = () => {
-  const stats = [
-    {
-      label: "Total Market Cap",
-      value: "$2.48T",
-      change: "+2.4%",
-    },
 
-    {
-      label: "24h Volume",
-      value: "$84.2B",
-      change: "+5.8 %",
-    },
 
-    { label: "BTC Dominance", value: "52.4%", change: "-0.3 % " },
-
-    { label: "Active Cryptos", value: "12,847", change: "+24" },
-  ];
+  
 
   return (
     <div className="">
       <div className="grid grid-cols-[repeat(4,_1fr)] gap-5 mb-8">
-        {stats.map((item) => (
+        {StatsData.map((item) => (
           <StatCard stat={item} key={item.label} />
         ))}
       </div>
